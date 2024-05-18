85890a900441f4c512f797f778c0c69fbccf58b9
RobKraft <rob@robkraft.org>
2024-05-18 10:26:49 -0500

    turnstile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,34 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
-export default function Home() {
+'use client';
+import { useRef, useState } from 'react';
+import styles from './page.module.css'
+import { Turnstile, TurnstileInstance } from '@marsidev/react-turnstile'; // <------------- Importing Turnstile component
+ 
+const Home = () => {
+ 
+  const [canSubmit, setCanSubmit] = useState(false); // <------------- We will use this state to enable submit button if Turnstile says all is well.
+  const refTurnstile = useRef<TurnstileInstance>(null); // <------------- Ref to Turnstile component. We will use this to reset Turnstile after each submit.
+ 
+  const handleSubmit = async () => {
+    refTurnstile.current?.reset(); // <------------- After each submit, recycling turnstile for next usage.
+    console.log('submitted!');
+  }
+ 
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <div className={styles.pageCenter}>
+      <form onSubmit={handleSubmit}>
+        <div><input type="text" placeholder="username" /></div>
+        <div><input type="password" placeholder="password" /></div>
+        <div><button type='submit' disabled={!canSubmit}>Login</button></div>
+        <br />
+        <Turnstile
+          id='turnstile-1'
+          ref={refTurnstile}
+          siteKey='YOUR_SITE_KEY'
+          onSuccess={() => setCanSubmit(true)}
         />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </form>
+    </div>
+  )
 }
+ 
+export default Home;
